862e3b77861766a79591a5d931dc44eca6cf3cb4
YaphetKG <kebedey@renci.org>
2020-01-28 16:25:42 -0500

    removing pycache files

---
+++ KITCHEN/Automat/main.py
@@ -0,0 +1,21 @@
+
+from Automat.automat.config import config
+from Automat.automat.util.logutil import LoggingUtil
+from Automat.automat.server import app
+import uvicorn
+
+logger = LoggingUtil.init_logging(__name__,
+                                  config.get('logging_level'),
+                                  config.get('logging_format'),
+                                  config.get('logging_file_path')
+                                  )
+
+if __name__=='__main__':
+    logger.debug('[0] Starting web server')
+    web_server_host = config.get('WEB_HOST', '127.0.0.1')
+    web_server_port: int = int(config.get('WEB_PORT', 8081))
+    uvicorn.run(
+        app,
+        host=web_server_host,
+        port=web_server_port
+    )
