3e640144b199eeee7f4abe902835d8fc592461f9
YaphetKG <kebedey@renci.org>
2020-01-28 16:28:52 -0500

    automat server simplification

--- KITCHEN/Automat/automat/server.py
+++ KITCHEN/Automat/automat/server.py
@@ -1,30 +1,7 @@
-from starlette.requests import Request
 from Automat.automat.core import Automat
-import aiohttp
 
-
-# async def proxy_to_plater(request: Request):
-#     build_tag = request.path_params['build_tag']
-#     source_type = request.path_params['source_type']
-#     target_type = request.path_params['target_type']
-#     curie = request.path_params['curie']
-
+automat = Automat()
 
 async def app (scope, recieve, send):
     assert scope['type'] == 'http'
-    a = Automat()
-    await a.handle_request(scope, recieve, send)
-    # request = Request(scope, recieve)
-    # print(scope['path'])
-    # await send({
-    #     'type': 'http.response.start',
-    #     'status': 200,
-    #     'headers': [
-    #         [b'content-type', b'text/plain'],
-    #     ]
-    # })
-    # await send({
-    #     'type': 'http.response.body',
-    #     'body': b'',
-    # })
-
+    await automat.handle_request(scope, recieve, send)
