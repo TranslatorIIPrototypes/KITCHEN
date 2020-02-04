ff3b74e0c241caf69b3c899cb4a1d512b4b9c20c
YaphetKG <kebedey@renci.org>
2020-02-04 12:07:35 -0500

    reorganizing automat code.

--- KITCHEN/Automat/automat/core.py
+++ KITCHEN/Automat/automat/core.py
@@ -23,22 +23,7 @@ class Automat:
         self.config = config
         self.swagger_ui_html = Automat.setup_swagger_ui_html()
 
-    @staticmethod
-    def setup_swagger_ui_html():
-        env = Environment(
-            loader=FileSystemLoader(swagger_ui_3_path)
-        )
-        template = env.get_template('index.j2')
-        html_content = template.render(
-            title=f'Automat',
-            openapi_spec_url="./openapi.yml",
-        )
-        return html_content
-
-    async def handle_swagger_docs(self, scope, recieve, send):
-        html_response = HTMLResponse(content=self.swagger_ui_html, media_type='text/html')
-        await html_response(scope, recieve, send)
-
+    # /heartbeat
     async def handle_heartbeat(self, scope, receive, send):
         body = await Automat.read_body(receive)
         heart_beat_json = json.loads(body.decode('utf-8'))
@@ -51,99 +36,7 @@ class Automat:
         json_response = JSONResponse(current_state)
         await json_response(scope, receive, send)
 
-    @staticmethod
-    def parse_headers_to_dict(headers):
-        return {key[0].decode('utf-8'): key[1].decode('utf-8') for key in headers}
-
-    async def handle_route_to_backend(self, scope, receive, send, backend_server_url, path):
-        final_path = f'http://{backend_server_url}/{"/".join(path)}'
-        logger.debug(f'[0] proxing reqeust to {final_path}')
-        if scope['method'] == 'GET':
-            response = await async_get_json(final_path, Automat.parse_headers_to_dict(scope['headers']))
-        elif scope['method'] == 'POST':
-            body = await Automat.read_body(receive)
-            response = await async_post_json(
-                final_path,
-                Automat.parse_headers_to_dict(scope['headers']),
-                body
-            )
-        await Automat.send_json_response(scope, receive, send, response)
-
-    async def handle_request(self, scope, receive, send):
-        # get the backend we want to get to
-        path = list(filter(lambda x: x != '', scope['path'].split('/')))
-        logger.debug(f'{path}')
-        # redirect index to apidocs ??
-        if not path:
-            await self.handle_swagger_docs(scope, receive, send)
-            return
-
-        root_path = path[0]
-
-        # IF API DOCS WAS REQUESTED JUST RETURN THAT
-        if root_path == 'apidocs':
-            await self.handle_swagger_docs(scope, receive, send)
-            return
-
-        if root_path == 'openapi.yml':
-            await self.handle_open_api_yaml(scope, receive, send)
-            return
-
-        if root_path == 'heartbeat':
-            await self.handle_heartbeat(scope, receive, send)
-            return
-
-        backend_sever_url = self.registry.get_host_by_tag(root_path)
-        if backend_sever_url:
-            logger.debug(f'[0] found entry for backend server {root_path} --- {backend_sever_url}')
-            await self.handle_route_to_backend(scope, receive, send, backend_sever_url, path[1:])
-            return
-        try:
-            await self.handle_static_files(scope, receive, send)
-        except Exception as e:
-            logger.error(e)
-
-    @staticmethod
-    async def read_body(receive):
-        """
-        Read and return the entire body from an incoming ASGI message.
-        """
-        body = b''
-        more_body = True
-        while more_body:
-            message = await receive()
-            body += message.get('body', b'')
-            more_body = message.get('more_body', False)
-        return body
-
-    @staticmethod
-    async def handle_static_files(scope, receive, send):
-        static_file = StaticFiles(directory=swagger_ui_3_path)
-        await static_file(scope, receive, send)
-
-    @staticmethod
-    async def send_json_response(scope, receive, send, data):
-        json_response = JSONResponse(data)
-        await json_response(scope, receive, send)
-
-    @staticmethod
-    async def send_404_response(scope, receive, send):
-        json_response = JSONResponse({
-            'error': f'No registered backend servers on {scope["path"]}'
-        }, status_code=404)
-        await json_response(scope, receive, send)
-
-    @staticmethod
-    async def get_swagger_paths(server_url, timeout=5*6):
-        open_api_path = '/openapi.json'
-        full_path = f'http://{server_url}{open_api_path}'
-        response = await async_get_json(full_path, timeout=timeout)
-        if 'error' in response:
-            logger.error(response['error'])
-            return {}
-        logger.debug(response)
-        return response
-
+    # /openapi.yml
     async def handle_open_api_yaml(self, scope, receive, send):
         ### For each registered endpoint try and grab its open_api_spec.
         logger.debug(f'paths for open_api specs of each server')
@@ -173,6 +66,126 @@ class Automat:
         schema = yaml.dump(open_api_spec, default_flow_style=False).encode("utf-8")
         response = PlainTextResponse(schema)
         await response(scope, receive, send)
+
+    # /<x>/*
+    async def handle_route_to_backend(self, scope, receive, send, backend_server_url, path):
+        final_path = f'http://{backend_server_url}/{"/".join(path)}'
+        logger.debug(f'[0] proxing request to {final_path}')
+        if scope['method'] == 'GET':
+            response = await async_get_json(final_path, Automat.parse_headers_to_dict(scope['headers']))
+        elif scope['method'] == 'POST':
+            body = await Automat.read_body(receive)
+            response = await async_post_json(
+                final_path,
+                Automat.parse_headers_to_dict(scope['headers']),
+                body
+            )
+        await Automat.send_json_response(scope, receive, send, response)
+
+    # /registry
+    async def handle_registry(self, scope, receive, send):
+        await Automat.send_json_response(scope, receive, send, self.registry.get_registry())
+
+    # /<Swagger ui files>
+    @staticmethod
+    async def handle_static_files(scope, receive, send):
+        static_file = StaticFiles(directory=swagger_ui_3_path)
+        await static_file(scope, receive, send)
+
+    # /apidocs
+    async def handle_swagger_docs(self, scope, recieve, send):
+        html_response = HTMLResponse(content=self.swagger_ui_html, media_type='text/html')
+        await html_response(scope, recieve, send)
+
+    async def handle_request(self, scope, receive, send):
+        # get the backend we want to get to
+        path = list(filter(lambda x: x != '', scope['path'].split('/')))
+        logger.debug(f'{path}')
+        # redirect index to apidocs ??
+        if not path:
+            await self.handle_swagger_docs(scope, receive, send)
+            return
+
+        root_path = path[0]
+
+        # IF API DOCS WAS REQUESTED JUST RETURN THAT
+        if root_path == 'apidocs':
+            await self.handle_swagger_docs(scope, receive, send)
+            return
+
+        if root_path == 'openapi.yml':
+            await self.handle_open_api_yaml(scope, receive, send)
+            return
+
+        if root_path == 'heartbeat':
+            await self.handle_heartbeat(scope, receive, send)
+            return
+
+        if root_path == 'registry':
+            await self.handle_registry(scope, receive, send)
+            return
+
+        backend_sever_url = self.registry.get_host_by_tag(root_path)
+        if backend_sever_url:
+            logger.debug(f'[0] found entry for backend server {root_path} --- {backend_sever_url}')
+            await self.handle_route_to_backend(scope, receive, send, backend_sever_url, path[1:])
+            return
+        try:
+            await self.handle_static_files(scope, receive, send)
+        except Exception as e:
+            logger.error(e)
+
+    @staticmethod
+    def setup_swagger_ui_html():
+        env = Environment(
+            loader=FileSystemLoader(swagger_ui_3_path)
+        )
+        template = env.get_template('index.j2')
+        html_content = template.render(
+            title=f'Automat',
+            openapi_spec_url="./openapi.yml",
+        )
+        return html_content
+
+    @staticmethod
+    def parse_headers_to_dict(headers):
+        return {key[0].decode('utf-8'): key[1].decode('utf-8') for key in headers}
+
+    @staticmethod
+    async def read_body(receive):
+        """
+        Read and return the entire body from an incoming ASGI message.
+        """
+        body = b''
+        more_body = True
+        while more_body:
+            message = await receive()
+            body += message.get('body', b'')
+            more_body = message.get('more_body', False)
+        return body
+
+    @staticmethod
+    async def send_json_response(scope, receive, send, data):
+        json_response = JSONResponse(data)
+        await json_response(scope, receive, send)
+
+    @staticmethod
+    async def send_404_response(scope, receive, send):
+        json_response = JSONResponse({
+            'error': f'No registered backend servers on {scope["path"]}'
+        }, status_code=404)
+        await json_response(scope, receive, send)
+
+    @staticmethod
+    async def get_swagger_paths(server_url, timeout=5*6):
+        open_api_path = '/openapi.json'
+        full_path = f'http://{server_url}{open_api_path}'
+        response = await async_get_json(full_path, timeout=timeout)
+        if 'error' in response:
+            logger.error(response['error'])
+            return {}
+        logger.debug(response)
+        return response
 
 
 
